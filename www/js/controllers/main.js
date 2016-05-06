c655fea0cec5e38e39cbdd55ff610ce8789c4d51
CarlosV <carlos.vazquez@empowerlabs.com>
2016-05-06 13:24:17 -0500

    Phonegap

--- www/js/controllers/main.js
+++ www/js/controllers/main.js
@@ -2,10 +2,13 @@ app.controller("mainController", function ($scope){
 $scope.titulo="Programs";
 $scope.programas=[{"nombre":"Programa1","costo":"$300"},{"nombre":"Programa2","costo":"$700"},{"nombre":"Programa3","costo":"$1300"},{"nombre":"Programa1","costo":"$300"},{"nombre":"Programa2","costo":"$700"},{"nombre":"Programa3","costo":"$1300"}];
 $scope.page='pages/programs.html';
+$scope.programsPage='pages/programs.html';
+$scope.newProgramPage="pages/newProgram.html";
+
 
 $scope.showForm= function(){
-  $scope.page='pages/newProgram.html';
-  $scope.titulo="Create Program";
+  $scope.page=$scope.newProgramPage;
+  $scope.titulo="New Program";
 }
 
 });
